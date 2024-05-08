06cd3299b4782ea2701b65bec3326e93ff2c1ce7
Karl Wang <wangkang330@tju.edu.cn>
2024-05-08 17:47:10 +0800

    wuliuJiankong

--- js/mstp_map.js
+++ js/mstp_map.js
@@ -38,36 +38,36 @@ var bar_data23 = [[1,60000],[2,55000],[3,10000], [4,8000], [5,7000], [6,20000], 
 var bar_data24 = [[1,60200],[2,50900],[3,60000], [4,58000], [5,60000], [6,61000], [7,59000], [8,65000], [9,50900],[10,60100],[11,50000],[12,60000]];
 
 var bar_datas = {
-                  'xining':[{data : bar_data1,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data2,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data3,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'xining':[{data : bar_data1,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data2,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data3,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'haibei':[{data : bar_data4,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data5,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data6,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'haibei':[{data : bar_data4,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data5,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data6,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'haidong':[{data : bar_data7,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data8,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data9,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
-                  'haixi':[{data : bar_data10,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data11,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data12,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'haidong':[{data : bar_data7,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data8,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data9,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'haixi':[{data : bar_data10,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data11,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data12,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'hainan':[{data : bar_data13,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data14,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data15,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'hainan':[{data : bar_data13,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data14,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data15,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'yushu':[{data : bar_data16,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data17,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data18,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'yushu':[{data : bar_data16,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data17,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data18,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'huangnan':[{data : bar_data19,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data20,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data21,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
+                  'huangnan':[{data : bar_data19,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data20,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data21,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}],
 
-                  'guoluo':[{data : bar_data22,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data23,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
-                            {data : bar_data24,label: '照明能耗',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}]
+                  'guoluo':[{data : bar_data22,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data23,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}},
+                            {data : bar_data24,label: '温度正常',bars : {show : true, barWidth : 0.3, order : 1,fill:1,lineWidth: 0,fillColor: 'rgba(255,255,255,0.6)'}}]
                 };
 function clk_map(area){
   //$(".absolute-area div").removeClass("active")
@@ -169,7 +169,7 @@ $(function () {
 
         barData.push({
                 data : data1,
-                label: '照明能耗',
+                label: '温度正常',
                 bars : {
                         show : true,
                         barWidth : 0.3,
@@ -181,7 +181,7 @@ $(function () {
         });
         barData.push({
                 data : data2,
-                label: '空调能耗',
+                label: '湿度正常',
                 bars : {
                         show : true,
                         barWidth : 0.3,
@@ -193,7 +193,7 @@ $(function () {
         });
         barData.push({
                 data : data3,
-                label: '负载能耗',
+                label: '未检测出',
                 bars : {
                         show : true,
                         barWidth : 0.3,
@@ -266,14 +266,16 @@ $(function () {
 //Pie Chart
 $(function(){
     var pieData = [
-                    {data: 1, color: 'rgba(255,200,255,0.8)',label:'照明能耗'},
-                    {data: 2, color: 'rgba(255,255,200,0.8)',label:'空调能耗'},
-                    {data: 3, color: 'rgba(200,255,255,0.8)',label:'负载能耗'}
+                    {data: 1, color: 'rgba(255,200,255,0.8)',label:'温度异常'},
+                    {data: 2, color: 'rgba(255,255,200,0.8)',label:'湿度异常'},
+                    {data: 3, color: 'rgba(200,200,255,0.8)',label:'未检测出'},
+                    {data: 4, color: 'rgba(200,0,200,0.8)',label:'温湿度正常'}
+
                 ];
     var donutPieData = [
-        {data: 1, color: 'rgba(255,200,255,0.8)',label:'其他油机'},
-        {data: 2, color: 'rgba(255,255,200,0.8)',label:'机房油机'},
-        {data: 3, color: 'rgba(200,255,255,0.8)',label:'基站油机'}
+        {data: 1, color: 'rgba(255,0,0,0.8)',label:'超速'},
+        {data: 2, color: 'rgba(200,200,255,0.8)',label:'未检测出'},
+        {data: 3, color: 'rgba(0,255,0,0.8)',label:'未超速'}
     ];
     if($('#donut-chart')[0]){
         donutChart = $.plot('#donut-chart', donutPieData, {
